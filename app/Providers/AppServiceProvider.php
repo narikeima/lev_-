def11c6fb4a307621011adc828f3edadd0faff71
narikeima <narikei_0327@icloud.com>
2024-09-28 03:24:34 +0900

    Revert "Breeze認証機能実装"
    This reverts commit dbeb42d95993a6d321ed40660ba128bf0f299c46.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,9 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Paginator::useBootstrap();
-        
-        \URL::forceScheme('https');
-         $this->app['request']->server->set('HTTPS','on');
     }
 }
